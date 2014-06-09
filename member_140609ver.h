1d705fd1b4dae12de4a7e53fdc2a807f859d76ec
Shinyoung07 <sypark9307@gmail.com>
2014-06-09 23:27:24 +0900

    수정/삭제 메인메뉴로 빼기
    각 함수에서 입력없을 때 다시입력받기 처리

---
+++ member_140609ver.h
@@ -0,0 +1,38 @@
+#ifndef __MEMBER_H__
+#define __MEMBER_H__
+
+typedef struct student_node{
+	int userid;
+	char name[10];
+	char address[80];
+	char phone[14];
+}student;
+
+
+void readMemberData(void);
+void showMenu(void);
+void returnToMenu(void);
+
+void printAll(void);
+void printPage(int curPageNum, int lastPageNum);
+
+void addNewMember(void);
+void phoneFormCheck(int memberId);
+
+void deleteMember(int memberIdx);
+void editMember(int memberIdx);
+
+void searchMember(int fromwhere);
+void printCertainMember(int memberNum);
+void searchSubmenu(int memberIdx);
+
+
+void saveData(void);
+void exitProgram(void);
+
+void gotoxy(int x, int y);
+void memoryExtension();
+void textcolor(unsigned short color);
+
+
+#endif // !__MEMBER_H__
